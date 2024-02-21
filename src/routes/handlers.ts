71579a1bf8cd377171e3c699cf8d0ff37c4acee1
Renc17 <cangaprogram@gmail.com>
2024-02-21 02:37:31 +0200

    fix: route handlers querying

--- src/routes/handlers.ts
+++ src/routes/handlers.ts
@@ -2,24 +2,30 @@ import { MongooseAdapter } from '../Mongoose';
 import { Request, Response } from 'express';
 
 export class RouteHandlers {
-  constructor(private readonly adapter: MongooseAdapter) {}
-
   async getCompanies(req: Request, res: Response) {
-    let { query } = req.params as { [key: string]: any };
-    const { skip, limit } = req.query as { [key: string]: any };
-    if (!query && query.length === 0) {
-      query = {};
+    const { skip, limit, name, domain, hsId } = req.query as {
+      [key: string]: any;
+    };
+    let filer: { $and: any[] } = { $and: [{}] };
+    if (hsId) {
+      filer.$and.push({ 'hs.id': hsId });
     }
-    const results = await this.adapter.getCompanies(query, {
-      skip: skip ?? 0,
-      limit: limit ?? 50,
+    if (name) {
+      filer.$and.push({ name });
+    }
+    if (domain) {
+      filer.$and.push({ domain });
+    }
+    const results = await MongooseAdapter.getInstance().getCompanies(filer, {
+      skip: parseInt(skip) ?? 0,
+      limit: parseInt(limit) ?? 50,
     });
     res.json(results);
   }
 
   async getCompanyById(req: Request, res: Response) {
     const id = req.params.id;
-    const result = await this.adapter.getCompanyById({ id });
+    const result = await MongooseAdapter.getInstance().getCompanyById({ id });
     res.json(result);
   }
 }
