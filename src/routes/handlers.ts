7c1b3ac97b0c39164b177ef1903aba877f9048aa
Renc17 <cangaprogram@gmail.com>
2024-02-21 02:46:09 +0200

    feat: api error handling

--- src/routes/handlers.ts
+++ src/routes/handlers.ts
@@ -2,7 +2,7 @@ import { MongooseAdapter } from '../Mongoose';
 import { Request, Response } from 'express';
 
 export class RouteHandlers {
-  async getCompanies(req: Request, res: Response) {
+  getCompanies(req: Request, res: Response) {
     const { skip, limit, name, domain, hsId } = req.query as {
       [key: string]: any;
     };
@@ -16,16 +16,21 @@ export class RouteHandlers {
     if (domain) {
       filer.$and.push({ domain });
     }
-    const results = await MongooseAdapter.getInstance().getCompanies(filer, {
-      skip: parseInt(skip) ?? 0,
-      limit: parseInt(limit) ?? 50,
-    });
-    res.json(results);
+
+    MongooseAdapter.getInstance()
+      .getCompanies(filer, {
+        skip: parseInt(skip) ?? 0,
+        limit: parseInt(limit) ?? 50,
+      })
+      .then(companies => res.json(companies))
+      .catch(err => res.status(500).json({ error: err.message }));
   }
 
-  async getCompanyById(req: Request, res: Response) {
+  getCompanyById(req: Request, res: Response) {
     const id = req.params.id;
-    const result = await MongooseAdapter.getInstance().getCompanyById({ id });
-    res.json(result);
+    MongooseAdapter.getInstance()
+      .getCompanyById({ id })
+      .then(company => res.json(company))
+      .catch(err => res.status(500).json({ error: err.message }));
   }
 }
